341b85a7895650d91f7ad3281e2cc6f62ecd7d1a
zhuyunpeng <13338642387@163.com>
2018-06-03 23:28:38 +0800

    cors

--- src/main/java/com/zyp/springvueserver/security/WebConfig.java
+++ src/main/java/com/zyp/springvueserver/security/WebConfig.java
@@ -1,72 +1,44 @@
 package com.zyp.springvueserver.security;
 
-import com.zyp.springvueserver.config.CorsConfig;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
-@EnableWebMvc
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig extends WebMvcConfigurationSupport {
 
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        //允许全部请求跨域
-//        registry.addMapping("/**").allowedOrigins("*")
-//                .allowCredentials(true)
-//                .allowedHeaders(CorsConfiguration.ALL)
-//                .allowedMethods("GET", "POST", "DELETE", "PUT")
-//                .maxAge(3600);
+//    /**
+//     允许任何域名使用
+//     允许任何头
+//     允许任何方法（post、get等）
+//     */
+//    private CorsConfiguration buildConfig() {
+//        CorsConfiguration corsConfiguration = new CorsConfiguration();
+//        // addAllowedOrigin 不能设置为* 因为与 allowCredential 冲突
+//        corsConfiguration.addAllowedOrigin("*");
+//        corsConfiguration.addAllowedHeader("*");
+//        corsConfiguration.addAllowedMethod("*");
+//        // allowCredential 需设置为true
+//        corsConfiguration.setAllowCredentials(true);
+//        return corsConfiguration;
 //    }
-
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         //添加拦截器
         registry.addInterceptor(new JwtInterceptor()).excludePathPatterns("/user/login");
     }
 
-    //    private CorsConfiguration buildConfig() {
-//        CorsConfiguration corsConfiguration = new CorsConfiguration();
-//        List<String> list = new ArrayList<>();
-//        list.add("*");
-//        corsConfiguration.setAllowedOrigins(list);
-//    /*
-//    // 请求常用的三种配置，*代表允许所有，当时你也可以自定义属性（比如header只能带什么，只能是post方式等等）
-//    */
-//        corsConfiguration.addAllowedOrigin("*");
-//        corsConfiguration.addAllowedHeader("*");
-//        corsConfiguration.addAllowedMethod("*");
-//        return corsConfiguration;
-//    }
-//
+
 //    @Bean
 //    public CorsFilter corsFilter() {
 //        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 //        source.registerCorsConfiguration("/**", buildConfig());
 //        return new CorsFilter(source);
 //    }
-    @Bean
-    public FilterRegistrationBean corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
-        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-        bean.setOrder(0);
-        return bean;
-    }
+
 
 }
